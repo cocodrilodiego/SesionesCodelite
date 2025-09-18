4d70f39d69d1cca87c4ada56f69edb980648b2c4
cocodrilodiego <diego.villap@edu.uah.es>
2025-09-18 18:19:46 +0200

    ModEjemplo01

---
+++ EjemplosSesion1/Ejemplo01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+    int x;
+    std::cout <<"Escriba un numero: ";
+    std::cin >> x;
+    std::cout <<"El numero introducido es: " << x << std::endl;
+    
+    return 0;
+}
